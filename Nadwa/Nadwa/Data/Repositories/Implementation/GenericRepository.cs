c46450c1105f4a4dfc6d0e1603684e61baed7aeb
Kareem Magdy <kareemmagdy1175@gmail.com>
2025-05-08 19:04:35 +0300

    Add Generic repository.

---
+++ Nadwa/Nadwa/Data/Repositories/Implementation/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Nadwa.Data.Repositories.Interface;
+
+namespace Nadwa.Data.Repositories.Implementation;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class {
+    private readonly ApplicationDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public GenericRepository(ApplicationDbContext context) {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+
+    public async Task<T?> GetByIdAsync(string id, Expression<Func<T, bool>>? predicate = null) {
+        IQueryable<T> query = _dbSet;
+        if (predicate is not null) {
+            query = query.Where(predicate);
+        }
+
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(
+        Expression<Func<T, bool>>? predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+        Expression<Func<T, object>>? groupBy = null)
+    {
+        IQueryable<T> query = _context.Set<T>();
+
+        if (predicate != null)
+            query = query.Where(predicate);
+
+        if (groupBy != null)
+            query = query.GroupBy(groupBy).SelectMany(g => g);
+
+        if (orderBy != null)
+            query = orderBy(query);
+
+        return await query.ToListAsync();
+    }
+    public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+    public void Update(T entity) {
+        _dbSet.Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+
+    public void Remove(T entity) => _dbSet.Remove(entity);
+}
---
+++ Nadwa/Nadwa/Data/Repositories/Interface/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Nadwa.Data.Repositories.Interface;
+
+public interface IGenericRepository<T> where T : class {
+    Task<T?> GetByIdAsync(string id, Expression<Func<T, bool>>? predicate);
+
+    Task<IEnumerable<T>> GetAllAsync(
+        Expression<Func<T, bool>>? predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+        Expression<Func<T, object>>? groupBy = null
+    );
+
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+}
