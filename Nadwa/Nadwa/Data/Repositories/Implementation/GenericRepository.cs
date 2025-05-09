2ce824065f7b65176f338f2106dfb598b87c806c
Kareem Magdy <kareemmagdy1175@gmail.com>
2025-05-10 00:17:46 +0300

    Update GenericRepository with pagination functionality.

--- Nadwa/Nadwa/Data/Repositories/Implementation/GenericRepository.cs
+++ Nadwa/Nadwa/Data/Repositories/Implementation/GenericRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Nadwa.Data.Repositories.Interface;
 using Nadwa.Services.Caching;
+using Nadwa.Utilites;
 using NuGet.Protocol;
 
 namespace Nadwa.Data.Repositories.Implementation;
@@ -63,6 +64,36 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class {
             query = orderBy(query);
 
         var result = await query.ToListAsync();
+        _redisCacheService.SetData(cacheKey, result);
+
+        return result;
+    }
+
+    public async Task<IEnumerable<T>> GetPagedAsync(int page = 1, int pageSize = 4,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, Expression<Func<T, bool>>? predicate = null,
+        Expression<Func<T, object>>? groupBy = null) {
+        var cacheKey = $"{typeof(T).Name}_All_" +
+                       $"{page}" +
+                       $"{predicate?.Body.ToString()}_" +
+                       $"{orderBy?.Method.Name}_" +
+                       $"{groupBy?.Body.ToString()}";
+
+        var cached = _redisCacheService.GetData<IEnumerable<T>>(cacheKey);
+
+        if (cached is not null) return cached;
+
+        IQueryable<T> query = _context.Set<T>();
+
+        if (predicate is not null)
+            query = query.Where(predicate);
+
+        query = orderBy is not null ? orderBy(query) : query.OrderBy(e => 0);
+
+        var result = await query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
         _redisCacheService.SetData(cacheKey, result);
 
         return result;
--- Nadwa/Nadwa/Data/Repositories/Interface/IGenericRepository.cs
+++ Nadwa/Nadwa/Data/Repositories/Interface/IGenericRepository.cs
@@ -11,6 +11,14 @@ public interface IGenericRepository<T> where T : class {
         Expression<Func<T, object>>? groupBy = null
     );
 
+    Task<IEnumerable<T>> GetPagedAsync(
+        int page = 1,
+        int pageSize = 10,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+        Expression<Func<T, bool>>? filter = null,
+        Expression<Func<T, object>>? groupBy = null);
+    
+
     Task AddAsync(T entity);
     void Update(T entity);
     void Remove(T entity);
