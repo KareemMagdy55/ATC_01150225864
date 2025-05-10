9271645886af1617e9ef05c48320e638e9b048a1
Kareem Magdy <kareemmagdy1175@gmail.com>
2025-05-10 03:28:07 +0300

    Implement UnitOfWork + update bootstrap lib.

---
+++ Nadwa/Nadwa/Data/Repositories/Interface/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Nadwa.Data.Repositories.Interface;
+
+public interface IUnitOfWork : IDisposable, IAsyncDisposable {
+    Task<int> CompleteAsync();
+    public IEventRepository EventRepository { get; }
+    public IApplicationUserRepository ApplicationUserRepository { get; }
+}
--- Nadwa/Nadwa/Program.cs
+++ Nadwa/Nadwa/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Nadwa.Data;
+using Nadwa.Data.Repositories.Interface;
 using Nadwa.Models;
 using Nadwa.Services.ApplicationUser;
 using Nadwa.Services.Caching;
@@ -30,6 +31,7 @@ builder.Services.AddRazorPages();
 
 
 builder.Services.AddScoped<IRedisCacheService, RedisCacheService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IApplicationUserService, ApplicationUserService>();
 builder.Services.AddScoped<IEventService, EventService>();
 
